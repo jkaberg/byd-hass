f017c39e3d0601f4e06ff329f46af42a27fffd4e
Joel Kåberg <joel@kaberg.me>
2025-06-24 06:28:03 +0000

    kill any running instances

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ pkg install -y jq
 
 ### Setup the environment ###
 
-# Boot dir is an part of Termux:Boot init 
+# Boot dir is part of Termux:Boot init
 mkdir -p "$BOOT_DIR"
 curl -sL -o "$BOOT_DIR/run.sh" "$BASE_URL/run.sh"
 chmod +x "$BOOT_DIR/run.sh"
@@ -22,8 +22,12 @@ mkdir -p "$SCRIPTS_DIR"
 scripts=("poll_diplus_nohup.sh" "update_abrp_nohup.sh" "update_hass_nohup.sh")
 
 for script in "${scripts[@]}"; do
+  # Kill any running instances of the script
+  pkill -f "$SCRIPTS_DIR/$script"
+
+  # Download the latest version
   curl -sL -o "$SCRIPTS_DIR/$script" "$BASE_URL/scripts/$script"
   chmod +x "$SCRIPTS_DIR/$script"
 done
 
-bash $BOOT_DIR/run.sh
+bash "$BOOT_DIR/run.sh"
